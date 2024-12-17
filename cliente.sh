0fa440c9fe3bd28f753bef266df484834301fe4b
Albert <albertferdiaz1@gmail.com>
2024-12-17 14:27:18 +0100

    Actualizacion del contenido antes de navidad

--- cliente.sh
+++ cliente.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 #################CLIENTE#################
 
+IP_SERVIDOR=localhost
+PORT=7777
+
 echo "LSTP Cliente (Lecuga Transfer Protocol)"
 
 echo "1. SEND HEADER"
 
-echo  "LSTP_1" | nc localhost 7777
+echo  "LSTP_1" | nc $IP_SERVIDOR $PORT
 #
 
 echo "2. LISTENER OK_HEADER"
 
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
 
 echo "6. CHECK OK_HEADER"
 
@@ -28,5 +31,36 @@ fi
 
 echo "7. SEND FILE_NAME"
 
-echo "FILE_NAME saludo.ogg" | nc localhost 7777
+echo "FILE_NAME saludo.ogg" | nc $IP_SERVIDOR $PORT
 
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+
+echo "8. LISTEN"
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_NAME" ]
+then
+	
+	echo "ERROR 2: FILE_NAME error en el envio"
+	exit 2
+
+fi
+
+echo "12. SEND FILE DATA"
+cat saludo.ogg | nc $IP_SERVIDOR $PORT  
+
+echo "13. LISTEN OK/KO_FILE_DATA"
+
+DATA= `nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_DATA" ]
+then
+	echo "ERROR 3: Error al enviar los datos
+	exit 3"
+
+fi
+
+echo "Fin"
+exit 0
--- server.sh
+++ server.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 ##################SERVIDOR###################
 
+PORT=7777
+IP_CLIENT=localhost
 
 echo "LSTP Server (Lechuga Speacker Transfer Protocol))"
 echo "0. LISTEN"
 
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
 
 ###
 echo "3. CHECK"
@@ -14,15 +16,43 @@ if [ "$DATA" != "LSTP_1" ]
 then
 	echo "ERROR 1: Header mal formado"
 
-	echo "KO_HEADER" | nc localhost 7777
+	echo "KO_HEADER" | nc $IP_CLIENT $PORT
 	
 fi
 
 ####
 echo "4. SEND OK_HEADER"
-echo "OK_HEADER" | nc localhost 7777
+echo "OK_HEADER" | nc $IP_CLIENT $PORT
 
 #####
 echo "5. LISTEN FILE_NAME"
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
 
+echo "9. CHECK FILE_NAME"
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then 
+	echo "ERROR 2: FILE_NAME incorrecto"
+	
+	echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
+	exit 2
+
+fi
+
+FILE_NAME=`echo $DATA | cut -d " " -f 2`
+
+
+echo "10. SEND OK_FILE_NAME"
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+echo "11. LISTEN FILE DATA"
+
+nc -l $PORT > ~/cliente/$FILE_NAME 
+
+
+
+echo "FIN"
+exit 0
